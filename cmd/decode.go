05f412471280ad7e38e8996285c52fad121e8704
Paul Daniel Iway <paul.daniel.iway@gmail.com>
2018-08-16 09:46:59 +0800

    added decoder

--- cmd/decode.go
+++ cmd/decode.go
@@ -11,11 +11,11 @@ import (
 )
 
 var (
-	encoder = new(translate.TextToBase64)
+	decoder = new(translate.Base64ToText)
 )
 
-var cmdEncode = &cobra.Command{
-	Use:   "encode [path to secrets.yml]",
+var cmdDecode = &cobra.Command{
+	Use:   "decode [path to secrets.yml]",
 	Short: "Encodes the literal values in a secrets file to base64", Long: `print is for printing anything back to the screen. For many years people have printed back to the screen.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		path, err := cmd.Flags().GetString("file")
@@ -37,15 +37,15 @@ var cmdEncode = &cobra.Command{
 		// data map - as in a map of the data values from the yaml
 		dm := make(map[string]string)
 		for k, v := range s.Data {
-			dm[k] = encoder.Translate(v)
+			dm[k] = decoder.Translate(v)
 		}
 		reader.Seek(0, 0)
 		secreto.Write(reader, dm)
 	},
 }
 
-func newEncodeCmd() *cobra.Command {
-	cmdEncode.Flags().StringP("file", "f", "", "the path to the secrets manifest")
-	cmdEncode.MarkFlagRequired("file")
-	return cmdEncode
+func newDecodeCmd() *cobra.Command {
+	cmdDecode.Flags().StringP("file", "f", "", "the path to the secrets manifest")
+	cmdDecode.MarkFlagRequired("file")
+	return cmdDecode
 }
--- cmd/root.go
+++ cmd/root.go
@@ -23,5 +23,6 @@ func init() {
 	Commands = append(Commands,
 		cmdVersion,
 		newEncodeCmd(),
+		newDecodeCmd(),
 	)
 }
